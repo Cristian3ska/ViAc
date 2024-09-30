c03209a920a88c02ca7e86b7dd9bfb3d2129fcfa
Cristian3ska <cris.escalante.t@gmail.com>
2024-09-30 03:04:38 -0600

    archivos de yii

---
+++ views/site/index.php
@@ -0,0 +1,20 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'ViAc';
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
+        <h1 class="display-4">ViAc</h1>
+
+        <p class="lead">Visual Activities</p>
+
+        <p>
+            <a class="btn btn-lg btn-primary" href="">Inicia Sesión</a>
+            <a class="btn btn-lg btn-success" href="">Registrate</a>
+        </p>
+    </div>
+
+</div>
